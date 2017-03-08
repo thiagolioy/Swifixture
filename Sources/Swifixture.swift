c03aee7db38ef1d66261ff718ce6fd4fecef0bcb
Thiago Lioy <Thiago.Lioy@CSSP-580.local>
2017-03-08 09:51:14 -0300

    Should be able to load json from file using swifixture

---
+++ Sources/Swifixture.swift
@@ -0,0 +1,31 @@
+//
+//  Swifixture.swift
+//  Swifixture
+//
+//  Created by Thiago Lioy on 08/03/17.
+//  Copyright © 2017 tplioy. All rights reserved.
+//
+
+import Foundation
+struct Swifixture {
+    
+    let data: Data
+    let json: String
+    
+    init?(file: String, withExtension fileExt: String = "json", in bundle:Bundle = Bundle.main) {
+        guard let path = bundle.path(forResource: file, ofType: fileExt) else {
+            return nil
+        }
+        let pathURL = URL(fileURLWithPath: path)
+        do {
+            data = try Data(contentsOf: pathURL, options: .dataReadingMapped)
+            if let decoded = NSString(data: data, encoding: 0) as? String {
+                json = decoded
+            } else {
+                return nil
+            }
+        } catch{
+            return nil
+        }
+    }
+}
--- Tests/SwifixtureSpec.swift
+++ Tests/SwifixtureSpec.swift
@@ -15,8 +15,26 @@ class SwifixtureSpec: QuickSpec {
     override func spec() {
 
         describe("SwifixtureSpec") {
-            it("works") {
-                expect("Swifixture") == "Swifixture"
+            context("existing json") {
+                var swifixture: Swifixture?
+                beforeEach {
+                    let bundle = Bundle(for: type(of: self))
+                    swifixture = Swifixture(file: "example", in: bundle)
+                }
+                
+                it("should be able to create Swifixture instance") {
+                    expect(swifixture).toNot(beNil())
+                }
+                it("should have valid json") {
+                    expect(swifixture!.json).toNot(beEmpty())
+                }
+            }
+            
+            context("no file in path") {
+                it("should be able to create Swifixture instance") {
+                    let swifixture = Swifixture(file: "notInPath")
+                    expect(swifixture).to(beNil())
+                }
             }
         }
 
